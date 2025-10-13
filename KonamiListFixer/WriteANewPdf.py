362b9d3ec1ff0dd1a8536df9835a90b12f226344
GuilhermeLossio <86495756+GuilhermeLossio@users.noreply.github.com>
2025-10-12 23:13:42 -0300

    Final fixes on base code
    Now i just need to finish the deploy.

--- KonamiListFixer/WriteANewPdf.py
+++ KonamiListFixer/WriteANewPdf.py
@@ -1,11 +1,13 @@
 from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import letter
+from reportlab.lib.pagesizes import letter, A4
 from pdfrw import PdfReader, PdfWriter, PageMerge
 from flask import url_for
+import io
+from datetime import datetime
 
 
 
-def fill_konami_decklist(data):
+def fill_konami_decklist(data, deck_data):
     input_pdf_file = "./src/static/pdfs/decklist.pdf"  # PDF em branco
     output_pdf_file = "./src/static/pdfs/decklist_test_filled.pdf"
 
@@ -16,9 +18,9 @@ def fill_konami_decklist(data):
     # -----------------------------
     # Informações Pessoais
     # -----------------------------
-    c.drawString(102, 720, data.get("first_middle_name", ""))  # First & Middle Name
-    c.drawString(102, 695, data.get("last_name", ""))          # Last Name
-    c.drawString(400, 695, data.get("country_of_residency", ""))  # Country of Residency
+    c.drawString(102, 720, data.get("first_middle_names", ""))  # First & Middle Name
+    c.drawString(102, 695, data.get("last_names", ""))          # Last Name
+    c.drawString(400, 695, data.get("country_residency", ""))  # Country of Residency
     ##c.drawString(520, 735, data.get("last_name_initial", ""))  # Last Name Initial
     c.drawString(345, 670, data.get("event_name", ""))         # Event Name
     c.setFont("Helvetica", 24)
@@ -30,13 +32,27 @@ def fill_konami_decklist(data):
     for i, char in enumerate(text):
         c.drawString(x + i * (c.stringWidth(char, "Helvetica", 12) + spacing), y, char)
         
-    if "event_month" in data and "event_day" in data and "event_year" in data:
-        c.drawString(347, 718, data["event_month"][0].zfill(2))
-        c.drawString(401, 718, data["event_month"][1].zfill(2))
-        c.drawString(457, 718, data["event_day"][0].zfill(2))
-        c.drawString(495, 718, data["event_day"][1].zfill(2))
-        """ for i, ch in enumerate(data["event_year"]):
-            c.drawString(530 + i*15, 735, ch) """
+        event_date_str = data.get("event_date", "")
+    try:
+        event_dt = datetime.strptime(event_date_str, "%Y-%m-%d")
+        event_day = f"{event_dt.day:02}"
+        event_month = f"{event_dt.month:02}"
+        event_year = str(event_dt.year)
+        
+        # Desenha mês
+        for i, ch in enumerate(event_day):
+            c.drawString(347 + i*15, 718, ch)
+
+        # Desenha dia
+        for i, ch in enumerate(event_month):
+            c.drawString(401 + i*15, 718, ch)
+
+        # Desenha ano
+        for i, ch in enumerate(event_year):
+            c.drawString(455 + i*18, 718, ch)
+
+    except Exception as e:
+        print(f"Erro ao processar event_date: {event_date_str} -> {e}")
     c.setFont("Helvetica", 12)
     
 
@@ -44,46 +60,132 @@ def fill_konami_decklist(data):
     # Cartas
     # -----------------------------
     line_height = 17
-
-    # Monster Cards (posição inicial aproximada)
+    total_genesys_points = 0
+    total_main_quantity = 0
+    
+    # --- MONSTERS CARDS ---
+    max_monster_quantity = 0
+    max_monster_points = 0
+    data["main"] = deck_data["main"]
     y_monster_start = 636
-    for i, card in enumerate(data.get("monster_cards", [])):
-        y = y_monster_start - i*line_height
-        c.drawString(40, y, str(card.get("qty", "")))       # QTY
-        c.drawString(62, y, card.get("name", ""))          # Nome
-        c.drawString(205, y, str(card.get("pts", "")))      # PTS
+    monster_index = 0
+    for card in data.get("main", []):
+        if "monster" in card.get("type", "").lower():
+            y = y_monster_start - monster_index * line_height
+            quantity = card.get("quantity", 0)
+            points = card.get("genesys_points", 0)
 
-    # Spell Cards
+            c.drawString(40, y, str(quantity))  # QTY
+            draw_fitted_text(c, 62, y, card.get("name", ""), max_width=130)
+            c.drawString(205, y, str(points*quantity))  # PTS
+
+            max_monster_quantity += quantity
+            max_monster_points += points*quantity
+            total_main_quantity += quantity
+            total_genesys_points += points*quantity
+            monster_index += 1
+
+    # Mostra total de monstros
+    c.drawString(40, 330, str(max_monster_quantity))
+    c.drawString(205, 330, str(max_monster_points))
+
+    # --- SPELL CARDS ---
+    spell_quantity = 0
+    spell_points = 0
     y_spell_start = 636
-    for i, card in enumerate(data.get("spell_cards", [])):
-        y = y_spell_start - i*line_height
-        c.drawString(225, y, str(card.get("qty", "")))      # QTY
-        c.drawString(245, y, card.get("name", ""))          # Nome
-        c.drawString(390, y, str(card.get("pts", "")))      # PTS
+    spell_index = 0
+    for card in data.get("main", []):
+        if "spell" in card.get("type", "").lower():
+            y = y_spell_start - spell_index * line_height
+            quantity = card.get("quantity", 0)
+            points = card.get("genesys_points", 0)
 
-    # Trap Cards
+            c.drawString(225, y, str(quantity))
+            draw_fitted_text(c, 245, y, card.get("name", ""), max_width=130)
+            c.drawString(390, y, str(points*quantity))
+
+            spell_quantity += quantity
+            spell_points += points*quantity
+            total_main_quantity += quantity
+            total_genesys_points += points*quantity
+            spell_index += 1
+
+    c.drawString(225, 330, str(spell_quantity))
+    c.drawString(390, 330, str(spell_points))
+    
+
+    # --- TRAP CARDS ---
+    trap_quantity = 0
+    trap_points = 0
     y_trap_start = 636
-    for i, card in enumerate(data.get("trap_cards", [])):
-        y = y_trap_start - i*line_height
-        c.drawString(410, y, str(card.get("qty", "")))      # QTY
-        c.drawString(430, y, card.get("name", ""))          # Nome
-        c.drawString(575, y, str(card.get("pts", "")))      # PTS
+    trap_index = 0
+    for card in data.get("main", []):
+        if "trap" in card.get("type", "").lower():
+            y = y_trap_start - trap_index * line_height
+            quantity = card.get("quantity", 0)
+            points = card.get("genesys_points", 0)
 
-    # Side Deck
+            c.drawString(410, y, str(quantity))
+            draw_fitted_text(c, 430, y, card.get("name", ""), max_width=130)
+            c.drawString(575, y, str(points*quantity))
+
+            trap_quantity += quantity
+            trap_points += points*quantity
+            total_main_quantity += quantity
+            total_genesys_points += points*quantity
+            trap_index += 1
+
+    c.drawString(415, 330, str(trap_quantity))
+    c.drawString(580, 330, str(trap_points))
+
+    # --- SIDE DECK ---
+    side_quantity = 0
+    side_points = 0
     y_side_start = 295
+    data["side_deck"] = deck_data["side"]
     for i, card in enumerate(data.get("side_deck", [])):
-        y = y_side_start - i*line_height
-        c.drawString(40, y, str(card.get("qty", "")))
-        c.drawString(62, y, card.get("name", ""))
-        c.drawString(205, y, str(card.get("pts", "")))
+        y = y_side_start - i * line_height
+        quantity = card.get("quantity", 0)
+        points = card.get("genesys_points", 0)
 
-    # Extra Deck
+        c.drawString(40, y, str(quantity))
+        draw_fitted_text(c, 62, y, card.get("name", ""), max_width=130)
+        c.drawString(205, y, str(points*quantity))
+
+        side_quantity += quantity
+        side_points += points*quantity
+
+    c.drawString(40, 40, str(side_quantity))
+    c.drawString(205, 40, str(side_points))
+
+    # --- EXTRA DECK ---
+    extra_quantity = 0
+    extra_points = 0
     y_extra_start = 295
+    data["extra_deck"] = deck_data["extra"]
     for i, card in enumerate(data.get("extra_deck", [])):
-        y = y_extra_start - i*line_height
-        c.drawString(225, y, str(card.get("qty", "")))
-        c.drawString(245, y, card.get("name", ""))
-        c.drawString(390, y, str(card.get("pts", "")))
+        y = y_extra_start - i * line_height
+        quantity = card.get("quantity", 0)
+        points = card.get("genesys_points", 0)
+
+        c.drawString(225, y, str(quantity))
+        draw_fitted_text(c, 245, y, card.get("name", ""), max_width=130)
+        c.drawString(390, y, str(points*quantity))
+
+        extra_quantity += quantity
+        extra_points += points*quantity
+
+    c.drawString(225, 40, str(extra_quantity))
+    c.drawString(390, 40, str(extra_points))
+
+
+    # --- RESUMO GERAL ---
+    c.setFont("Helvetica-Bold", 16)
+    c.drawString(540, 670, f"{total_main_quantity}")
+    c.drawString(570, 670, f"{total_genesys_points}")
+    c.setFont("Helvetica-Bold", 12)
+    
+    
 
     c.save()
 
@@ -97,13 +199,44 @@ def fill_konami_decklist(data):
         overlay_page = overlay_pdf.pages[0]
         merger = PageMerge(page)
         merger.add(overlay_page).render()
+        
+    
+    # -----------------------------
+    # Salvar memorias em Bytes
+    # -----------------------------
+    buffer = io.BytesIO()
+    PdfWriter(buffer, trailer=template_pdf).write()
+    buffer.seek(0)
 
-    PdfWriter(output_pdf_file, trailer=template_pdf).write()
+    # This is a old code that save on memory
+    """ PdfWriter(output_pdf_file, trailer=template_pdf).write()
     #decklistPath = url_for('static', filename=output_pdf_file)
     print(f"PDF preenchido salvo em: {output_pdf_file}")
-    output_pdf_file = output_pdf_file.replace("./", "")
-    return output_pdf_file
+    output_pdf_file = output_pdf_file.replace("./", "") """
+    
+        
+    return buffer.getvalue()
 
+
+
+def draw_fitted_text(c, x, y, text, max_width, initial_font_size=10, font_name="Helvetica"):
+    """
+    Diminui a fonte até o texto caber dentro do max_width.
+    """
+    font_size = initial_font_size
+    c.setFont(font_name, font_size)
+    
+    # Calcula a largura do texto
+    text_width = c.stringWidth(text, font_name, font_size)
+    
+    # Diminui a fonte até caber
+    while text_width > max_width and font_size > 4:  # evita fonte minúscula demais
+        font_size -= 0.5
+        text_width = c.stringWidth(text, font_name, font_size)
+    
+    c.setFont(font_name, font_size)
+    c.drawString(x, y, text)
+    
 # -----------------------------
 # Teste
 # -----------------------------
@@ -112,74 +245,72 @@ if __name__ == "__main__":
         "first_middle_name": "João Pedro",
         "last_name": "Silva",
         "card_game_id": "1234567891",
-        "country_of_residency": "Brazil",
+        "country_residency": "Brazil",
         "last_name_initial": "S",
         "event_name": "Regional Tournament",
-        "event_month": "01",
-        "event_day": "15",
-        "event_year": "2024",
+        "event_date": "2030-01-20",
         "monster_cards": [
-            {"qty": 3, "name": "Blue-Eyes White Dragon", "pts": 10},
-            {"qty": 2, "name": "Dark Magician", "pts": 0},
-            {"qty": 3, "name": "Blue-Eyes White Dragon", "pts": 10},
-            {"qty": 2, "name": "Dark Magician", "pts": 0},
-            {"qty": 3, "name": "Blue-Eyes White Dragon", "pts": 10},
-            {"qty": 2, "name": "Dark Magician", "pts": 0},
-            {"qty": 3, "name": "Blue-Eyes White Dragon", "pts": 10},
-            {"qty": 2, "name": "Dark Magician", "pts": 0},
-            {"qty": 3, "name": "Blue-Eyes White Dragon", "pts": 10},
-            {"qty": 2, "name": "Dark Magician", "pts": 0},
-            {"qty": 3, "name": "Blue-Eyes White Dragon", "pts": 10},
-            {"qty": 2, "name": "Dark Magician", "pts": 0},
-            {"qty": 3, "name": "Blue-Eyes White Dragon", "pts": 10},
-            {"qty": 2, "name": "Dark Magician", "pts": 0},
-            {"qty": 3, "name": "Blue-Eyes White Dragon", "pts": 10},
-            {"qty": 2, "name": "Dark Magician", "pts": 0},
-            {"qty": 3, "name": "Blue-Eyes White Dragon", "pts": 10},
-            {"qty": 2, "name": "Dark Magician", "pts": 0},
+            {"quantity": 3, "name": "Blue-Eyes White Dragon", "pts": 10},
+            {"quantity": 2, "name": "Dark Magician", "pts": 0},
+            {"quantity": 3, "name": "Blue-Eyes White Dragon", "pts": 10},
+            {"quantity": 2, "name": "Dark Magician", "pts": 0},
+            {"quantity": 3, "name": "Blue-Eyes White Dragon", "pts": 10},
+            {"quantity": 2, "name": "Dark Magician", "pts": 0},
+            {"quantity": 3, "name": "Blue-Eyes White Dragon", "pts": 10},
+            {"quantity": 2, "name": "Dark Magician", "pts": 0},
+            {"quantity": 3, "name": "Blue-Eyes White Dragon", "pts": 10},
+            {"quantity": 2, "name": "Dark Magician", "pts": 0},
+            {"quantity": 3, "name": "Blue-Eyes White Dragon", "pts": 10},
+            {"quantity": 2, "name": "Dark Magician", "pts": 0},
+            {"quantity": 3, "name": "Blue-Eyes White Dragon", "pts": 10},
+            {"quantity": 2, "name": "Dark Magician", "pts": 0},
+            {"quantity": 3, "name": "Blue-Eyes White Dragon", "pts": 10},
+            {"quantity": 2, "name": "Dark Magician", "pts": 0},
+            {"quantity": 3, "name": "Blue-Eyes White Dragon", "pts": 10},
+            {"quantity": 2, "name": "Dark Magician", "pts": 0},
         ],
         "spell_cards": [
-            {"qty": 2, "name": "Monster Reborn", "pts": 10},
-            {"qty": 1, "name": "Pot of Greed", "pts": 0},
-            {"qty": 2, "name": "Monster Reborn", "pts": 10},
-            {"qty": 1, "name": "Pot of Greed", "pts": 0},
-            {"qty": 2, "name": "Monster Reborn", "pts": 10},
-            {"qty": 1, "name": "Pot of Greed", "pts": 0},
-            {"qty": 2, "name": "Monster Reborn", "pts": 10},
-            {"qty": 1, "name": "Pot of Greed", "pts": 0},
-            {"qty": 2, "name": "Monster Reborn", "pts": 10},
-            {"qty": 1, "name": "Pot of Greed", "pts": 0},
+            {"quantity": 2, "name": "Monster Reborn", "pts": 10},
+            {"quantity": 1, "name": "Pot of Greed", "pts": 0},
+            {"quantity": 2, "name": "Monster Reborn", "pts": 10},
+            {"quantity": 1, "name": "Pot of Greed", "pts": 0},
+            {"quantity": 2, "name": "Monster Reborn", "pts": 10},
+            {"quantity": 1, "name": "Pot of Greed", "pts": 0},
+            {"quantity": 2, "name": "Monster Reborn", "pts": 10},
+            {"quantity": 1, "name": "Pot of Greed", "pts": 0},
+            {"quantity": 2, "name": "Monster Reborn", "pts": 10},
+            {"quantity": 1, "name": "Pot of Greed", "pts": 0},
         ],
         "trap_cards": [
-            {"qty": 2, "name": "Mirror Force", "pts": 10},
-            {"qty": 1, "name": "Bottomless Trap Hole", "pts": 0},
-            {"qty": 2, "name": "Mirror Force", "pts": 10},
-            {"qty": 1, "name": "Bottomless Trap Hole", "pts": 0},
-            {"qty": 2, "name": "Mirror Force", "pts": 10},
-            {"qty": 1, "name": "Bottomless Trap Hole", "pts": 0},
-            {"qty": 2, "name": "Mirror Force", "pts": 10},
-            {"qty": 1, "name": "Bottomless Trap Hole", "pts": 0},
-            {"qty": 2, "name": "Mirror Force", "pts": 10},
-            {"qty": 1, "name": "Bottomless Trap Hole", "pts": 0},
+            {"quantity": 2, "name": "Mirror Force", "pts": 10},
+            {"quantity": 1, "name": "Bottomless Trap Hole", "pts": 0},
+            {"quantity": 2, "name": "Mirror Force", "pts": 10},
+            {"quantity": 1, "name": "Bottomless Trap Hole", "pts": 0},
+            {"quantity": 2, "name": "Mirror Force", "pts": 10},
+            {"quantity": 1, "name": "Bottomless Trap Hole", "pts": 0},
+            {"quantity": 2, "name": "Mirror Force", "pts": 10},
+            {"quantity": 1, "name": "Bottomless Trap Hole", "pts": 0},
+            {"quantity": 2, "name": "Mirror Force", "pts": 10},
+            {"quantity": 1, "name": "Bottomless Trap Hole", "pts": 0},
         ],
         "side_deck": [
-            {"qty": 1, "name": "Evenly Matched", "pts": 10},
-            {"qty": 1, "name": "Evenly Matched", "pts": 10},
-            {"qty": 1, "name": "Evenly Matched", "pts": 10},
-            {"qty": 1, "name": "Evenly Matched", "pts": 10},
-            {"qty": 1, "name": "Evenly Matched", "pts": 10},
-            {"qty": 1, "name": "Evenly Matched", "pts": 10},
-            {"qty": 1, "name": "Evenly Matched", "pts": 10},
-            {"qty": 1, "name": "Evenly Matched", "pts": 10},
-            {"qty": 1, "name": "Evenly Matched", "pts": 10},
-            {"qty": 1, "name": "Evenly Matched", "pts": 10},
-            {"qty": 1, "name": "Evenly Matched", "pts": 10},
-            {"qty": 1, "name": "Evenly Matched", "pts": 10},
-            {"qty": 1, "name": "Evenly Matched", "pts": 10},
-            {"qty": 1, "name": "Evenly Matched", "pts": 10},
+            {"quantity": 1, "name": "Evenly Matched", "pts": 10},
+            {"quantity": 1, "name": "Evenly Matched", "pts": 10},
+            {"quantity": 1, "name": "Evenly Matched", "pts": 10},
+            {"quantity": 1, "name": "Evenly Matched", "pts": 10},
+            {"quantity": 1, "name": "Evenly Matched", "pts": 10},
+            {"quantity": 1, "name": "Evenly Matched", "pts": 10},
+            {"quantity": 1, "name": "Evenly Matched", "pts": 10},
+            {"quantity": 1, "name": "Evenly Matched", "pts": 10},
+            {"quantity": 1, "name": "Evenly Matched", "pts": 10},
+            {"quantity": 1, "name": "Evenly Matched", "pts": 10},
+            {"quantity": 1, "name": "Evenly Matched", "pts": 10},
+            {"quantity": 1, "name": "Evenly Matched", "pts": 10},
+            {"quantity": 1, "name": "Evenly Matched", "pts": 10},
+            {"quantity": 1, "name": "Evenly Matched", "pts": 10},
         ],
         "extra_deck": [
-            {"qty": 1, "name": "Number 39: Utopia", "pts": 0},
+            {"quantity": 1, "name": "Number 39: Utopia", "pts": 0},
         ],
     }
 
--- KonamiListFixer/app.py
+++ KonamiListFixer/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session, send_file
 import os
+import io
 import requests
 import time
+import secrets, string
 from collections import Counter
 
 import WriteANewPdf
@@ -18,8 +20,11 @@ app = Flask(
     template_folder="../src/templates",
     static_folder="../src/static"
 )
-app.secret_key = "supersecretkey"
+
+alphabet = string.ascii_letters + string.digits + "!@#$%^&*()-_=+"
+app.secret_key = ''.join(secrets.choice(alphabet) for _ in range(50))
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.secret_key = "muda_essa_para_algo_secreto"  # necessário para session
 
 # Função para validar extensão
 def allowed_file(filename):
@@ -65,16 +70,19 @@ def get_card_info(card_id):
                 "name": card["name"],
                 "image_url": card["card_images"][0]["image_url"],
                 "quantity": 1,
-                "genesys_points": card['misc_info'][0]['genesys_points']
+                "genesys_points": card['misc_info'][0]['genesys_points'],
+                "type" : card["type"]
             }
     return {"name": f"Unknown ({card_id})", "image_url": "", "quantity": 1, "genesys_points": 0}
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    deck_data = None
+    deck_data = {}
     loading = False
     submit_done = True
     decklistPath = ""
+    decklist_ready = False
+
 
     if request.method == "POST":
         if "deckFile" not in request.files:
@@ -96,14 +104,13 @@ def index():
 
             sections = parse_ydk(file_path)
 
-            # Contar repetições e gerar deck_data
-            deck_data = {}
             for section, ids in sections.items():
                 counter = Counter(ids)
                 deck_data[section] = [
                     {**get_card_info(card_id), "quantity": qty} for card_id, qty in counter.items()
                 ]
             submit_done = False
+            decklist_ready = True
             
             dataUser = {
                 "first_middle_names": request.form.get("first_middle_names", "").strip(),
@@ -113,13 +120,30 @@ def index():
                 "country_residency": request.form.get("country_residency", "").strip(),
                 "event_name": request.form.get("event_name", "").strip(),
             }
+            session["deck_data_user"] = dataUser
+            session["decklist"] = deck_data
             
-            decklistPath = WriteANewPdf.fill_konami_decklist(dataUser)
+            decklistPath = WriteANewPdf.fill_konami_decklist(dataUser, deck_data)
             
         else:
             flash("Invalid file type. Only .ydk allowed.")
             return redirect(request.url)
-    return render_template("index.html", deck_data=deck_data, loading=loading, submit_done=submit_done, decklistPath=decklistPath)
+    return render_template("index.html", deck_data=deck_data, loading=loading, submit_done=submit_done,decklist_ready=decklist_ready)
+
+# rota que serve o PDF (gera sob demanda)
+@app.route("/decklist")
+def decklist():
+    dataUser = session.get("deck_data_user")
+    decklist = session.get("decklist")
+    if not dataUser:
+        return "Nenhum PDF gerado ainda", 404
+
+    # WriteANewPdf.fill_konami_decklist deve retornar bytes (como vc já tem)
+    pdf_bytes = WriteANewPdf.fill_konami_decklist(dataUser, decklist)
+    return send_file(io.BytesIO(pdf_bytes),
+                     mimetype="application/pdf",
+                     as_attachment=False,
+                     download_name="decklist.pdf")
 
 if __name__ == "__main__":
     app.run(debug=True)
